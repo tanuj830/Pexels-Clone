ff89343e86d768b01fe70ff8e5333e48b9cd43ba
tanuj <tanujbhatt644@gmail.com>
2023-03-02 22:22:01 +0530

    package.json file updated...!

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cc28f47352713b26"; }
+/******/ 		__webpack_require__.h = function() { return "1a5378edb1702570"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
