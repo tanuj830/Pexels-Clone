8c7ef6ab6c3f6664a52afffc8ce2a031dec12ce2
tanuj <tanujbhatt644@gmail.com>
2023-03-03 16:47:45 +0530

    project page updated...!

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,9 +7,10 @@ const axios = require("axios");
 import {useEffect} from 'react'
 
 const Header = () => {
-    const [data, setData] = React.useState([])
-    const [randomData, setRandomData] = React.useState([])
+    const [data, setData] = React.useState({})
+    const [randomData, setRandomData] = React.useState({})
     const [input, setInput] = React.useState("")
+    const [count, setCount] = React.useState(1)
   
     
     useEffect(() => {
@@ -18,12 +19,54 @@ const Header = () => {
             headers:{
               Authorization: 'E9fxGa0pPsmw9rqhmg1f3Zji6rjfe5rVBBWODadJkyDiQNigYjRjGySL'
             }
-          }).then(res=>setRandomData(res.data.photos)).catch(err=>console.log(err))
-    }, [])
-    
+          }).then(res=>setRandomData(res.data)).catch(err=>console.log(err))
+        }, [])
+
 
     const handleChange = (e) =>{
       setInput(e.target.value)
+    }
+  
+    const handlePreviousSearched = () =>{
+
+      axios.get(data.prev_page,{
+                headers:{
+                  Authorization: 'E9fxGa0pPsmw9rqhmg1f3Zji6rjfe5rVBBWODadJkyDiQNigYjRjGySL'
+                }
+              }).then(res=>setData(res.data)).catch(err=>console.log(err))
+        console.log(data)
+      }
+      
+      const handleNextSearched = () =>{
+      axios.get(data.next_page,{
+                headers:{
+                  Authorization: 'E9fxGa0pPsmw9rqhmg1f3Zji6rjfe5rVBBWODadJkyDiQNigYjRjGySL'
+                }
+              }).then(res=>setData(res.data)).catch(err=>console.log(err))
+        console.log(data)
+      
+    }
+
+
+    // Curated Page Pagination
+    const handlePreviousRand = () =>{
+
+      axios.get(randomData.prev_page,{
+                headers:{
+                  Authorization: 'E9fxGa0pPsmw9rqhmg1f3Zji6rjfe5rVBBWODadJkyDiQNigYjRjGySL'
+                }
+              }).then(res=>setRandomData(res.data)).catch(err=>console.log(err))
+        console.log(data)
+      }
+      
+      const handleNextRand = () =>{
+      axios.get(randomData.next_page,{
+                headers:{
+                  Authorization: 'E9fxGa0pPsmw9rqhmg1f3Zji6rjfe5rVBBWODadJkyDiQNigYjRjGySL'
+                }
+              }).then(res=>setRandomData(res.data)).catch(err=>console.log(err))
+        console.log(data)
+      
     }
 
 
@@ -32,7 +75,8 @@ const Header = () => {
         headers:{
           Authorization: 'E9fxGa0pPsmw9rqhmg1f3Zji6rjfe5rVBBWODadJkyDiQNigYjRjGySL'
         }
-      }).then(res=>setData(res.data.photos)).catch(err=>console.log(err))
+      }).then(res=>setData(res.data)).catch(err=>console.log(err))
+      console.log(data)
     }
 
 
@@ -76,9 +120,9 @@ const Header = () => {
     <div className='container'>
 <div className='row mt-5'>
 {
-  data.length > 0 ?
-  data.map(con=>(
-  <div key={con.id} className='col-12 col-md-4 mt-5'>
+  data.photos ?
+  data.photos.map(con=>(
+  <div key={con.id} className='col-12 col-md-4 mt-3'>
     <div className='box'>
     <img className='img-fluid original_img' src={con.src.original} alt="" />
     <div className='author_name d-flex justify-content-between'>
@@ -89,6 +133,19 @@ const Header = () => {
     </div>
   )): null
 }  
+
+{
+  data.photos ?
+  <div className='d-flex justify-content-center align-items-center p-3 mt-3'>
+<ul className="pagination">
+  {/* Pagination */}
+    <li className="page-item" onClick={handlePreviousSearched}><a className="page-link text-secondary" href="#">Previous</a></li>
+    <li className="page-item"><a className="page-link text-secondary" href="#">{data.page}</a></li>
+    <li className="page-item" onClick={handleNextSearched}><a className="page-link text-secondary" href="#">Next</a></li>
+  </ul>
+  </div>
+  : null
+}
 </div>
 </div>
 
@@ -98,9 +155,9 @@ const Header = () => {
     <h3 className='text-center fs-1 text-secondary lead'>Random Curated Pictures</h3>
 <div className='row mt-5'>
 {
-  randomData.length > 0 ?
-  randomData.map(con=>(
-  <div key={con.id} className='col-12 col-md-4 mt-5'>
+  randomData.photos ?
+  randomData.photos.map(con=>(
+  <div key={con.id} className='col-12 col-md-4 mt-3'>
     <div className='box'>
     <img className='img-fluid original_img' src={con.src.original} alt="" />
     <div className='author_name d-flex justify-content-between'>
@@ -110,6 +167,18 @@ const Header = () => {
     </div>
     </div>
   )): null
+}
+{
+  randomData.photos ?
+  <div className='d-flex justify-content-center align-items-center p-3 mt-3 '>
+    <ul className="pagination">
+  {/* Pagination */}
+    <li className="page-item " onClick={handlePreviousRand}><a className="page-link text-secondary" href="#">Previous</a></li>
+    <li className="page-item"><a className="page-link text-secondary" href="#">{randomData.page}</a></li>
+    <li className="page-item" onClick={handleNextRand}><a className="page-link text-secondary" href="#">Next</a></li>
+  </ul>
+  </div>
+  : null
 }
 </div>
 </div>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,6 @@ import Document, {
   Head,
   Main,
   NextScript,
-  DocumentContext,
-  DocumentInitialProps,
 } from 'next/document'
 
 
@@ -15,8 +13,6 @@ export default function MyDocument() {
       <Head>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
-
-  {/* @import url('https://fonts.googleapis.com/css2?family=Akaya+Telivigala&family=Amatic+SC:wght@700&family=Changa&family=Monoton&family=Poppins&family=Shadows+Into+Light&family=Shantell+Sans:wght@300&family=Work+Sans&display=swap'); */}
       </Head >
       <body>
         <Main />
