5342be77d4a5a86b458caf52300a53e5aa3a4f46
tanuj <tanujbhatt644@gmail.com>
2023-03-03 16:49:12 +0530

    project page updated...!

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,9 @@ export default function MyDocument() {
       <Head>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
+      <style>
+      @import url('https://fonts.googleapis.com/css2?family=Akaya+Telivigala&family=Amatic+SC:wght@700&family=Changa&family=Monoton&family=Poppins&family=Shadows+Into+Light&family=Shantell+Sans:wght@300&family=Work+Sans&display=swap');
+      </style>
       </Head >
       <body>
         <Main />
